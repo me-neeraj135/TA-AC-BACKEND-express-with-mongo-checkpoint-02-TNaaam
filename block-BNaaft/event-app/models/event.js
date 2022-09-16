47bd0c54b783bf87ea47b535d943df643edafc24
neeraj <me.neeraj1455@gmail.com>
2022-09-16 15:28:17 +0530

    add date on remark

--- block-BNaaft/event-app/models/event.js
+++ block-BNaaft/event-app/models/event.js
@@ -6,13 +6,13 @@ let Schema = mongoose.Schema;
 
 let eventSchema = new Schema(
   {
-    title: { type: String, required: true },
-    summary: { type: String },
+    title: { type: String, required: true, trim: true },
+    summary: { type: String, trim: true },
     host: { type: String, required: true },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     categories: [{ type: String, required: true }],
-    location: { type: String },
+    location: { type: String, trim: true },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
 
